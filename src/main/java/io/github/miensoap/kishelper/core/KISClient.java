482da38c74eed9728bf1a2ede18032b0f8d25946
miensoap <miensop51@gmail.com>
2024-08-04 11:07:34 +0900

    Feat. use QueryMap, cosnts

--- src/main/java/io/github/miensoap/kishelper/core/KISClient.java
+++ src/main/java/io/github/miensoap/kishelper/core/KISClient.java
@@ -3,16 +3,23 @@ package io.github.miensoap.kishelper.core;
 import feign.Feign;
 import feign.gson.GsonDecoder;
 import io.github.miensoap.kishelper.data.Response.AccessTokenResponse;
-import io.github.miensoap.kishelper.data.consts.Path;
+import io.github.miensoap.kishelper.data.Response.Price;
+import io.github.miensoap.kishelper.data.consts.ApiPath;
 import io.github.miensoap.kishelper.data.consts.TradingID;
 import io.github.miensoap.kishelper.data.request.ApiAuth;
+import io.github.miensoap.kishelper.data.request.QueryParams;
 import io.github.miensoap.kishelper.data.request.RequestHeader;
-import io.github.miensoap.kishelper.data.request.RequestUrl;
 import io.github.miensoap.kishelper.domain.DailyStockPriceInfo;
 import io.github.miensoap.kishelper.util.ConfigLoader;
 
 import java.util.List;
 import java.util.Map;
+
+import static io.github.miensoap.kishelper.data.consts.QueryParamKey.APPLY_MODIFIED_PRICE;
+import static io.github.miensoap.kishelper.data.consts.QueryParamKey.EXCHANGE_CODE;
+import static io.github.miensoap.kishelper.data.consts.QueryParamKey.INQUIRY_DATE;
+import static io.github.miensoap.kishelper.data.consts.QueryParamKey.PERIOD_DIVISION;
+import static io.github.miensoap.kishelper.data.consts.QueryParamKey.STOCK_SYMBOL;
 
 public class KISClient {
 
@@ -31,7 +38,7 @@ public class KISClient {
         this.auth = auth;
         this.kis = Feign.builder()
                 .decoder(new GsonDecoder())
-                .target(KoreaInvestmentApi.class, Path.KOREA_INVESTMENT_API_PROD);
+                .target(KoreaInvestmentApi.class, ApiPath.KOREA_INVESTMENT_API_PROD);
         if (!auth.isAccessTokenPresent()) getAccessToken();
     }
 
@@ -43,20 +50,21 @@ public class KISClient {
     }
 
     public List<DailyStockPriceInfo> getOverseasDailyPrice(String exchange, String symbol, boolean modified) {
-        String url = RequestUrl.ofOverseasPrice()
-                .path("quotations")
-                .path("dailyprice")
-                .param("EXCD", exchange)
-                .param("SYMB", symbol)
-                .param("GUBN", "0")
-                .param("BYMD", "")
-                .param("MODP", modified ? "1" : "0")
+        Map<String, String> params = new QueryParams()
+                .addParam(EXCHANGE_CODE, exchange)
+                .addParam(STOCK_SYMBOL, symbol)
+                .addParam(PERIOD_DIVISION, "0")
+                .addParam(INQUIRY_DATE, "") // Today
+                .addParam(APPLY_MODIFIED_PRICE, modified ? "1" : "0")
                 .build();
+
 
         Map<String, Object> headers = RequestHeader.basicBuilder(auth)
                 .tradingId(TradingID.OVERSEAS_DAILY_PRICE)
                 .build().toMap();
 
-        return kis.getDailyPrice(url, headers).getPrices();
+
+        Price dailyPrice = kis.getDailyPrice(params, headers);
+        return dailyPrice.getPrices();
     }
 }
---
+++ src/main/java/io/github/miensoap/kishelper/data/request/QueryParams.java
@@ -0,0 +1,34 @@
+package io.github.miensoap.kishelper.data.request;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static io.github.miensoap.kishelper.data.consts.QueryParamKey.USER_AUTH_INFO;
+
+public class QueryParams {
+    private final Map<String, String> parameters = new HashMap<>();
+
+    public QueryParams addParam(String key, String value) {
+        parameters.put(key, value);
+        return this;
+    }
+
+    /**
+     * 기본정보 (사용자 인증 정보 공백) 을 추가한 뒤 unmodifiableMap 반환
+     * @return QueryMap
+     */
+    public Map<String, String> build() {
+        this.addParam(USER_AUTH_INFO, "");
+        return Collections.unmodifiableMap(this.parameters);
+    }
+
+    public String toString() {
+        String paramString = this.parameters.entrySet().stream()
+                .map((param) -> param.getKey() + "=" + param.getValue())
+                .collect(Collectors.joining("&"));
+
+        return "?" + paramString;
+    }
+}
